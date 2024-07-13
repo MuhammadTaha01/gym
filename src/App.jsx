aa56967b3ac581528e2a62266536191f2ba850ef
Muhammad Taha <tahafaisal9292@gmail.com>
2024-07-14 02:17:41 +0500

    Counter was added to website

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,39 @@ import Pricing from './componentsJSX/pricing';
 import About from './componentsJSX/about';
 import Contact from './componentsJSX/contact';
 import Footer from './componentsJSX/footer';
+import Counter from './componentsJSX/counter'
 import { Element } from 'react-scroll';
 
 function App() {
   return (
     <>
+      {/* For Navbar */}
       <Navbar />
+      
+        {/* For Home */}
         <Element name='Home'>
           <Header/>
         </Element>
+
+        {/* For Pricing */}
         <Element name='Pricing'>
           <Pricing/>
         </Element>
+        
+        {/* For Customer Counter */}
+        <Counter/>
+      
+        {/* For Contact-Us */}
         <Element name='ContactUs'>
           <Contact/>
         </Element>
+
+        {/* For About-Us */}
         <Element name='AboutUs'>
           <About/>
         </Element>
+      
+      {/* For Footer */}
       <Footer />
     </>
   );
---
+++ src/componentsJSX/counter.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import CountUp from 'react-countup'
+import ScrollTrigger from 'react-scroll-trigger'
+import '../componentsCSS/counter.css'
+
+const counter = () => {
+    let [counterOn, setCounterOn] = useState(false)
+
+    return (
+        <ScrollTrigger onEnter={()=>setCounterOn(true)} onExit={()=>setCounterOn(false)}>
+            <div className='Counter-Container'>
+                <hr />
+                {/* <div className="Counter-Text">
+                    <h1> Happy Clients </h1>
+                </div> */}
+                <div className="Counter-Data-Container">
+                    <div className="Counter1">
+                        <h1> {counterOn && <CountUp start={6} end={150} duration={5}/>} + </h1>
+                        <h3> Happy Customers</h3>
+                    </div>
+                    <div className="Counter2">
+                        <h1> {counterOn && <CountUp start={0} end={6} duration={5}/>} + </h1>
+                        <h3>Years Of Experience</h3>
+                    </div>
+                    <div className="Counter3">
+                        <h1> {counterOn && <CountUp start={13} end={90} duration={5}/>} : {counterOn && <CountUp start={0} end={10} duration={5}/>} </h1>
+                        <h3>Ratio Of Males & Females in gym</h3>
+                    </div>
+                </div>
+                
+                <hr />
+            </div>
+        </ScrollTrigger>
+  )
+}
+
+export default counter
