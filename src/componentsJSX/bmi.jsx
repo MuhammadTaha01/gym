43017f9b66f4151c44c8379c933fa7c0329afcbe
Muhammad Taha <tahafaisal9292@gmail.com>
2024-07-15 00:29:21 +0500

    BMI Calculator was made functional

--- src/componentsJSX/bmi.jsx
+++ src/componentsJSX/bmi.jsx
@@ -1,29 +1,46 @@
 import React from 'react'
 import '../componentsCSS/bmi.css'
 
-const bmi = () => {
-  return (
-    <div className='Bmi-Container'>
+const Bmi = () => {
 
-        <hr />
+    const CheckBmi = () => {
+        const feet = document.getElementById('feet').value;
+        const inches = document.getElementById('inches').value;
+        const weight = document.getElementById('weight').value;
 
-        <div className="Bmi-Text">
-            <h1>Calculate Your Body-Mass-Index</h1>
-        </div>
-        <div className="Bmi-Inputs">
-            <input placeholder='Enter Your Weight' type="text" name="" id="" />
-            <div className="Bmi-Feet-Inches">
-                <input placeholder='Enter Your Height in Feets' className='Feet' type="text" name="" id="" />
-                <input placeholder='Enter Your Height in inches' className='Inches' type="text" name="" id="" />
+        if (!feet || !inches || !weight) {
+            alert("Please enter all fields.");
+            return;
+        }
+
+        const totalInches = (parseFloat(feet) * 12) + parseFloat(inches);
+        const heightInMeters = totalInches * 0.0254;
+        const weightInKg = parseFloat(weight);
+
+        const bmi = weightInKg / (heightInMeters ** 2);
+        document.getElementById('Result').innerText = bmi.toFixed(2);
+    }
+
+    return (
+        <div className='Bmi-Container'>
+            <hr />
+            <div className="Bmi-Text">
+                <h1>Calculate Your Body-Mass-Index</h1>
             </div>
-            <button>Check BMI</button>
+            <div className="Bmi-Inputs">
+                <input placeholder='Enter Your Weight In Kgs' type="text" id="weight" />
+                <div className="Bmi-Feet-Inches">
+                    <input placeholder='Enter Your Height in Feet' className='Feet' type="text" id="feet" />
+                    <input placeholder='Enter Your Height in Inches' className='Inches' type="text" id="inches" />
+                </div>
+                <button onClick={CheckBmi}>Check BMI</button>
+            </div>
+            <div className="Bmi-Result">
+                <h1>Your Body Mass Index is: <span id='Result'></span></h1>
+            </div>
+            <hr />
         </div>
-        <div className="Bmi-Result">
-            <h1>Your Body Mass Index is: </h1>
-        </div>
-        <hr />
-    </div>
-  )
+    )
 }
 
-export default bmi
+export default Bmi;
